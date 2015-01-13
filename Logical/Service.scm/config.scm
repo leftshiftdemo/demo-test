c1adcb130f88b0ea440701c6c5bb6b0a4315a3e4
John <john@johnharris.tv>
2015-01-13 16:52:32 -0600

    Initial Commit

---
+++ Logical/Service.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1421188996176" createdUser="john" id="-21cdccd6:14ae5761430:-7ebe" type="service_component_resource" updatedTimestamp="1421188996176" updatedUser="john" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
